8221183bb2a18c433aa3c1214873c8271b7f94c2
Prorog <proroog@gmail.com>
2021-02-16 14:27:13 +0100

    1.1 Player moves

--- public/js/main.js
+++ public/js/main.js
@@ -3,39 +3,105 @@ let level = 1;
 let ctx;
 let game;
 
+let player;
 
 let gameImages = {
-    player:{
+    player: {
         src: "public/img/player.png"
     },
-    grass:{
+    grass: {
         src: "public/img/grass.png"
     },
-    box:{
+    box: {
         src: "public/img/box.png"
     },
 }
 
-window.onload = ()=>{
-    game = document.querySelector("#game");
-    ctx = game.getContext('2d');
+window.onload = () => {
+    for (let i = 0; i < 64; i++) {
+        let div = document.createElement("div");
+        div.classList.add("box-block");
+        div.id = "box-" + i;
+        document.querySelector(".game-box_screen").append(div);
+    }
 
-    var player = new Player(0, 0, gameImages.player.src, 16, 16, 1);
+    player = new Player(5);
     player.drawPlayer();
+
+    document.onkeyup = (e) => {
+        if (e.keyCode == '38') {
+            player.movePlayer('up')
+        }
+        else if (e.keyCode == '40') {
+            player.movePlayer('down')
+        }
+        else if (e.keyCode == '37') {
+            player.movePlayer('left')
+        }
+        else if (e.keyCode == '39') {
+            player.movePlayer('right')
+        }
+    }
+}
+class Player {
+    constructor(pos) {
+        this.width = 8;
+        this.pos = pos;
+        this.url = gameImages.player.src;
+
+    }
+    drawPlayer() {
+        let image = document.createElement('img');
+        image.src = this.url;
+        image.position = "relative";
+        image.classList.add("game-item");
+        image.id = 'player'
+        document.querySelectorAll(".box-block")[this.pos].appendChild(image);
+    }
+    movePlayer(direction) {
+        switch (direction) {
+            case 'up':
+                this.move(-this.width);
+                break;
+            case 'down':
+                this.move(this.width);
+                break;
+            case 'right':
+                this.move(1);
+                break;
+            case 'left':
+                this.move(-1)
+                break;
+        }
+    }
+    move(dir) {
+        document.querySelectorAll(".box-block")[this.pos].removeChild(document.querySelectorAll(".box-block")[this.pos].children[0]);
+        this.pos += dir;
+        this.drawPlayer();
+    }
 }
 
-class Player{
-    constructor(x, y, url, width, height, speed){
-        this.x = x
-        this.y = y
-        this.url = url;
-        this.width = width;
-        this.height = height
-        this.speed = speed;
+/*
+class Game {
+    constructor(i) {
+        this.pos = i;
+        this.width = 8;
+        this.height = 8;
+
+        this.image = document.createElement('img');
+        this.image.src = "public/img/player.png";
+        this.image.classList.add("game-item");
     }
-    drawPlayer(){
-        let myImage = document.createElement('img');
-        myImage.src = this.url
-        ctx.drawImage(myImage,this.x,this.y,this.width,this.height)
+    drawPlayer() {
+        document.querySelectorAll(".box-block")[this.pos].appendChild(this.image);
+    }
+    move() {
+        if (this.pos < this.width) {
+            return;
+        }
+        document.querySelectorAll(".box-block")[this.pos].removeChild(this.image);
+        this.pos -= this.width;
+        this.drawPlayer();
     }
 }
+*/
