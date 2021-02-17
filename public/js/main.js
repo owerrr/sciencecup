f8fcc7d7b5b5cfd13e187ddffd5073c106efbedd
xAxee <50153178+xAxee@users.noreply.github.com>
2021-02-17 16:08:32 +0100

    1.5

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
 window.onload = () => {
-    runGame(1);
+    runGame(1)
+    document.querySelectorAll("li").forEach(e => {
+        e.onclick = () => {
+            document.querySelector(".level-select").classList.remove("level-select");
+
+            e.classList.add("level-select")
+            clearGame()
+            runGame(e.innerHTML)
+        }
+    })
 }
