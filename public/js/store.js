6af80427b3fa2c4a8e9d6846526ef41dd16b9fa8
PanZenon <karolwojtas2005@gmail.com>
2021-02-17 17:16:14 +0100

    v1.5
    store.html and store.css and store.js
    still in progress

---
+++ public/js/store.js
@@ -0,0 +1,10 @@
+let showOption = (val)=>{
+    switch(val){
+        case 'Colors':
+            alert('kolorek')
+            break;
+        case 'Hats':
+            alert('hats')
+            break;
+    }
+}
