48bd379ddea40c187db13a55eec5121245a75af5
PanZenon <karolwojtas2005@gmail.com>
2021-02-20 12:52:20 +0100

    Revert "Revert "won window and some fixes""
    This reverts commit 09d2b76ee383426aa396c68360f0be7897656a5f.

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,18 @@ window.onload = () => {
         document.querySelector('.loading-screen').classList.add('off')
         document.body.style.overflowY = 'visible'
       }, 1000)
+
+      document.querySelector('.next-level').onclick = ()=>{
+          game.clearGame()
+          if(parseInt(document.querySelector('.game-box').id)==8){
+            document.querySelector('.game-box').id = '0'
+          }
+          document.querySelector(".level-select").classList.toggle("level-select");
+          document.querySelectorAll(".level")[parseInt(document.querySelector('.game-box').id)].classList.toggle("level-select");
+          runGame(parseInt(document.querySelector('.game-box').id)+1)
+          document.querySelector('.won').style.display = 'none'
+          document.getElementById('moves').innerHTML=''
+      }
 }
 function getTime(){
     let s = game.stoper
